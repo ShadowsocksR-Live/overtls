0c51636a12478163b24a4dbc183e6ca8cad3c3c1
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-11-16 17:07:53 +0800

    minor

--- src/udprelay.rs
+++ src/udprelay.rs
@@ -23,14 +23,14 @@ pub async fn handle_s5_upd_associate(associate: Associate<UdpNeedReply>, config:
     // listen on a random port
     let udp_listener = UdpSocket::bind(SocketAddr::from((listen_ip, 0))).await;
     match udp_listener.and_then(|socket| socket.local_addr().map(|addr| (socket, addr))) {
-        Ok((listen_socket, listen_addr)) => {
+        Ok((listen_udp, listen_addr)) => {
             let mut reply_listener = associate
                 .reply(Reply::Succeeded, Address::SocketAddress(listen_addr))
                 .await?;
 
             let buf_size = MAX_UDP_RELAY_PACKET_SIZE.load(Ordering::Acquire)
                 - (command_max_serialized_len() - UdpHeader::max_serialized_len());
-            let listen_socket = Arc::new(AssociatedUdpSocket::from((listen_socket, buf_size)));
+            let listen_udp = Arc::new(AssociatedUdpSocket::from((listen_udp, buf_size)));
             let ctrl_addr = reply_listener.peer_addr()?;
 
             log::info!("[socks5] {} UDP associate listen_addr {}", ctrl_addr, listen_addr);
@@ -43,8 +43,8 @@ pub async fn handle_s5_upd_associate(associate: Associate<UdpNeedReply>, config:
 
             let res = tokio::select! {
                 _ = reply_listener.wait_until_closed() => Ok::<_, anyhow::Error>(()),
-                res = socks5_to_relay(listen_socket.clone(),ctrl_addr, pkt_send_tx) => res,
-                res = relay_to_socks5(listen_socket,ctrl_addr, pkt_recv_rx) => res,
+                res = socks5_to_relay(listen_udp.clone(),ctrl_addr, pkt_send_tx) => res,
+                res = relay_to_socks5(listen_udp,ctrl_addr, pkt_recv_rx) => res,
             };
 
             reply_listener.shutdown().await?;
@@ -68,20 +68,20 @@ pub const fn command_max_serialized_len() -> usize {
 }
 
 async fn socks5_to_relay(
-    socket: Arc<AssociatedUdpSocket>,
+    listen_udp: Arc<AssociatedUdpSocket>,
     ctrl_addr: SocketAddr,
     pkt_send_tx: Sender<(Bytes, Address)>,
 ) -> anyhow::Result<()> {
     loop {
         let buf_size = MAX_UDP_RELAY_PACKET_SIZE.load(Ordering::Acquire) - UdpHeader::max_serialized_len();
-        socket.set_max_packet_size(buf_size);
+        listen_udp.set_max_packet_size(buf_size);
 
-        let (pkt, frag, dst_addr, src_addr) = socket.recv_from().await?;
+        let (pkt, frag, dst_addr, src_addr) = listen_udp.recv_from().await?;
 
         if frag == 0 {
             log::debug!("[socks5] [{ctrl_addr}] [associate] [packet-to] {dst_addr}");
             let _ = pkt_send_tx.send((pkt, dst_addr)).await;
-            socket.connect(src_addr).await?;
+            listen_udp.connect(src_addr).await?;
             break;
         } else {
             log::warn!("[socks5] [{ctrl_addr}] [associate] [packet-to] socks5 UDP packet fragment is not supported");
@@ -91,9 +91,9 @@ async fn socks5_to_relay(
     loop {
         let buf_size = MAX_UDP_RELAY_PACKET_SIZE.load(Ordering::Acquire)
             - (command_max_serialized_len() - UdpHeader::max_serialized_len());
-        socket.set_max_packet_size(buf_size);
+            listen_udp.set_max_packet_size(buf_size);
 
-        let (pkt, frag, dst_addr) = socket.recv().await?;
+        let (pkt, frag, dst_addr) = listen_udp.recv().await?;
 
         if frag == 0 {
             log::debug!("[socks5] [{ctrl_addr}] [associate] [packet-to] {dst_addr}");
@@ -111,13 +111,13 @@ async fn socks5_to_relay(
 }
 
 async fn relay_to_socks5(
-    socket: Arc<AssociatedUdpSocket>,
+    listen_udp: Arc<AssociatedUdpSocket>,
     ctrl_addr: SocketAddr,
     mut pkt_recv_rx: Receiver<(Bytes, Address)>,
 ) -> anyhow::Result<()> {
     while let Some((pkt, src_addr)) = pkt_recv_rx.recv().await {
         log::debug!("[socks5] [{ctrl_addr}] [associate] [packet-from] {src_addr}");
-        socket.send(pkt, 0, src_addr).await?;
+        listen_udp.send(pkt, 0, src_addr).await?;
     }
     Ok(())
 }
