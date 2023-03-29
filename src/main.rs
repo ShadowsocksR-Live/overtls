f92ced1defb1572687e37bb7d3e038eff9085ee1
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-03-29 22:02:53 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<()> {
 
     dotenvy::dotenv().ok();
 
-    let level = if opt.verbose { "debug" } else { "info" };
+    let level = if opt.verbose { "trace" } else { "info" };
     let default = format!("{}={}", module_path!(), level);
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default)).init();
 
