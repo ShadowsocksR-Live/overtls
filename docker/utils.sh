6b11fb6eebb12ce2dc93fc890eb0227bf6f96d2c
chenxudong2020 <872603935@qq.com>
2024-01-20 19:53:41 +0800

    docker add  (#42)

---
+++ docker/utils.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echolog() {
+    echo -e "\033[32m[overtls log]\033[0m" $*
+}
+
+random_string_gen() {
+    local PASS=""
+    local MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" # "~!@#$%^&*()_+="
+    local LENGTH=$1
+    [ -z $1 ] && LENGTH="16"
+    while [ "${n:=1}" -le "$LENGTH" ]
+    do
+        PASS="$PASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+        let n+=1
+    done
+
+    echo ${PASS}
+}
