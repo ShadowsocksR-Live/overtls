f9587eb684517f7cec5f98f05212c79a987aec02
chenxudong2020 <872603935@qq.com>
2024-03-13 22:56:25 +0800

    開啓https3支持 僅支持tlsv1.3 同時支持overtls多個TUNNEL_PATH (#49)

--- docker/README.md
+++ docker/README.md
@@ -1,10 +1,12 @@
 # overtls
 ```bash
-docker run --restart always -p 80:80 -p 443:443  --name overtls -e TUNNEL_PATH=/secret-tunnel-path/ -v /cert:/cert -v /web:/web -itd chengxudong2020/overtls
+docker run --restart always -p 80:80 -p 443:443/tcp -p 443:443/udp --name overtls -e TUNNEL_PATH=/secret-tunnel-path/ -v /cert:/cert -v /web:/web -itd chengxudong2020/overtls
 ```
 
 # 参数说明
+- `-p` 当端口映射到宿主机443 tcp和udp端口的情况下支持http3
 - `-v 证书所在目录` 一定要映射到 /cert容器目录 证书目录中必须存在私钥 `privkey.pem` 公钥 `fullchain.pem` 若名字不对请改名
 - `-v web静态文件所在目录` 其中必须为包含 `index.php`, `index.html`, `index.htm`, `index.nginx-debian.html`
   中任何一个为默认首页, 请自行从网上下载之后放入目录重启容器或者新建容器提前備好，映射之后的容器的目录必须是 `/web`
-- `-e TUNNEL_PATH` 默认为 `/secret-tunnel-path/` 请務必修改, 否则造成迅速被 `GFW` 封鎖之后果自负
+- `-e TUNNEL_PATH` 默认格式为 `/secret-tunnel-path/` 或者 `/secret-tunnel-path/,/secret-tunnel-path2/,/secret-tunnel-path2/`.
+  请務必自行修改成复杂字符串, 否则造成迅速被 `GFW` 封鎖之后果自负
--- docker/run.sh
+++ docker/run.sh
@@ -37,47 +37,72 @@ checkindex(){
       is50x=1
     fi
 }
+
 initIndex(){
     checkindex
     if [ $isindex -eq 0 ]; then
-      echolog "不存在首页，则使用默认首页"
+      echolog "不存在首页, 则使用默认首页"
       \cp /index.html /web/index.html
     fi
     if [ $is50x -eq 0 ]; then
-     echolog "不存在50x错误页, 则使用默认50x错误页"
-     \cp /50x.html /web/50x.html
+      echolog "不存在50x错误页, 则使用默认50x错误页"
+      \cp /50x.html /web/50x.html
     fi
 }
 
 initConfig(){
-  rm -rf /etc/nginx/http.d/overtls.conf
-  cat > /etc/nginx/http.d/overtls.conf <<EOF
+  rm -rf /etc/nginx/conf.d/overtls.conf
+  cat > /etc/nginx/conf.d/overtls.conf <<EOF
   server {
         listen $HTTP_PORT default_server;
         listen [::]:$HTTP_PORT default_server;
+        server_name localhost;
         index index.php index.html index.htm index.nginx-debian.html;
         root  /web;
   }
   server {
         listen $HTTPS_PORT ssl default_server;
+        listen $HTTPS_PORT quic reuseport default_server;
         listen [::]:$HTTPS_PORT ssl default_server;
+        listen [::]:$HTTPS_PORT quic reuseport default_server;
+        http2 on;
         ssl_certificate       /cert/$SSL_PUBLIC;
         ssl_certificate_key   /cert/$SSL_KEY;
-        ssl_protocols         TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
-        ssl_ciphers           HIGH:!aNULL:!MD5;
+        ssl_protocols         TLSv1.3;
+        add_header Alt-Svc 'h3=":$HTTPS_PORT"; ma=86400';
         server_name           localhost;
         index index.php index.html index.htm index.nginx-debian.html;
         root  /web;
-        error_page 400 = /400.html;
+        error_page 400 403 502 = /index.html;
 
-        location $TUNNEL_PATH {
-            proxy_redirect off;
-            proxy_pass http://$OVERTLS_HOST:$OVERTLS_PORT;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade \$http_upgrade;
-            proxy_set_header Connection "upgrade";
-            proxy_set_header Host \$http_host;
+        location ~ \\.php$ {
+            
         }
+        
+        location = /index.html {
+          root  /web;
+        }
+EOF
+     TUNNEL_PATH_STRING="$TUNNEL_PATH"
+     OLD_IFS="$IFS"
+     IFS=','
+     for path in $TUNNEL_PATH; do
+          path="${path#"${path%%[![:space:]]*}"}"
+          path="${path%"${path##*[![:space:]]}"}"
+          cat >> /etc/nginx/conf.d/overtls.conf <<EOF
+            location $path {
+                proxy_redirect off;
+                proxy_pass http://$OVERTLS_HOST:$OVERTLS_PORT;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade \$http_upgrade;
+                proxy_set_header Connection "upgrade";
+                proxy_set_header Host \$http_host;
+            }
+EOF
+    done
+    IFS="$OLD_IFS"
+
+          cat >> /etc/nginx/conf.d/overtls.conf <<EOF
   }
 EOF
 
@@ -86,7 +111,8 @@ EOF
   cat > /default/config.json <<EOF
 {
     "remarks": "${identity}",
-    "tunnel_path": "$TUNNEL_PATH",
+    "tunnel_path": $(get_tunnel_path),
+
     "server_settings": {
         "forward_addr": "http://127.0.0.1:$HTTP_PORT",
         "listen_host": "$OVERTLS_HOST",
